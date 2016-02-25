763b3cc32bfb39404c903551278570a215de0fed
TT_dashen <ltt143@126.com>
2016-02-25 18:35:15 +0800

    完成

--- TTLoginDemo/TTLoginDemo/ViewController.m
+++ TTLoginDemo/TTLoginDemo/ViewController.m
@@ -10,13 +10,32 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *passWord;
+@property (nonatomic,assign) BOOL isSecure;
+
 @end
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)showBtnClick {
+    [_passWord resignFirstResponder];
+    self.isSecure = !self.isSecure;
+    NSString *str = self.passWord.text;
+    if(self.isSecure == NO){
+        [self.passWord setSecureTextEntry:YES];
+    }else{
+        self.passWord.text = @"";
+        [self.passWord setSecureTextEntry:NO];
+        self.passWord.text = str;
+    }
+    [_passWord becomeFirstResponder];
+    
 }
 
 - (void)didReceiveMemoryWarning {
